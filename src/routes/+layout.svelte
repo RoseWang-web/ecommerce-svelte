0d209ce21796c4b660a896daac04b9e97616d4c1
Rose <zxcasdqwe8898@gmail.com>
2023-07-19 18:19:34 -0600

    complete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
 	import '../app.css';
 </script>
-<h1 class="p-4 bg-slate-300">Welcome</h1>
-
-<slot />
-
-<footer class="p-4 bg-orange-300">footer is here</footer>
+<main class="flex flex-col min-h-screen">
+	<header>
+		<h1 class="p-4 bg-slate-300">Welcome</h1>
+	</header>
+	<div class="flex-grow">
+		<slot />
+	</div>
+	<footer class="p-4 bg-orange-300">
+		footer is here
+	</footer>
+</main>
--- src/routes/products/+page.server.js
+++ src/routes/products/+page.server.js
@@ -1,22 +1,7 @@
-export async function load(event){
- return{
-  products:[
-   {id:1,
-   name:'product 1',
-   price:9.99,
-   imageUrl:'some url'
- },
- {id:2,
-  name:'product 2',
-  price:0.99,
-  imageUrl:'some url'
-},
-{id:3,
-  name:'product 3',
-  price:3.3,
-  imageUrl:'some url'
-},
-
-  ]
+export async function load({fetch}){
+  const response = await fetch('/api/products');
+  const products = response.json();
+ return {
+    products,
  }
 }
